ed30b8c8f83efdddb525d87173cb18160ed9f4a2
Roman M. <romanmalaxof@yandex.ru>
2026-09-09 22:13:17 +0300

    Added realization of Triangle class

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+
+class Triangle {
+private:
+	double _side;
+	double _height;
+
+public:
+	Triangle(double side=1, double height=1);
+	double calculateArea() const;
+	double getSide() const;
+	double getHeight() const;
+	void setSide(double side);
+	void setHeight(double height);
+};
+
+Triangle::Triangle(double side, double height) {
+	setSide(side);
+	setHeight(height);
+}
+
+double Triangle::calculateArea() const {
+	return (_side * _height) / 2.0;
+}
+
+double Triangle::getSide() const {
+	return _side;
+}
+
+double Triangle::getHeight() const {
+	return _height;
+}
+
+void Triangle::setSide(double side) {
+	if (side <= 0) {
+		throw std::logic_error("Side must be positive");
+	}
+	_side = side;
+}
+
+void Triangle::setHeight(double height) {
+	if (height <= 0) {
+		throw std::logic_error("Height must be positive");
+	}
+	_height = height;
+}
+
+
 int main() {
-	double side, height;
+	double side, height, area;
+	Triangle t;
 	do {
 		std::cout << "Enter side: ";
 		std::cin >> side;
@@ -11,6 +59,7 @@ int main() {
 			std::cin.ignore(10000, '\n');
 		}
 	} while (std::cin.fail() || side <= 0);
+	t.setSide(side);
 	do {
 		std::cout << "Enter height: ";
 		std::cin >> height;
@@ -21,7 +70,9 @@ int main() {
 			std::cin.ignore(10000, '\n');
 		}
 	} while (std::cin.fail() || height <= 0);
-	double area = (side * height) / 2.0;
+	t.setHeight(height);
+	area=t.calculateArea();
 	std::cout << "Area of the triangle is " << area << std::endl;
 	return 0;
 }
+
